73755f4137633827284a4736ebee4f72da46537e
saeedane <dvdplay405@gmail.com>
2023-03-12 15:51:14 +0100

    update file css style

--- js/slider-init.js
+++ js/slider-init.js
@@ -13,7 +13,7 @@ jQuery(function() {
         autoplay: false,
         dots: false,
         nav: true,
-        navText: ["<img src='https://cdn.statically.io/gh/saeedane/sabaqpress/41fcaed7/img/icons/chevron-right.svg' width='24' height='24' >","<img src='https://cdn.statically.io/gh/saeedane/sabaqpress/41fcaed7/img/icons/chevron-left.svg' width='24' height='24' >"]
+        //navText: ["<img src='https://cdn.statically.io/gh/saeedane/sabaqpress/41fcaed7/img/icons/chevron-right.svg' width='24' height='24' >","<img src='https://cdn.statically.io/gh/saeedane/sabaqpress/41fcaed7/img/icons/chevron-left.svg' width='24' height='24' >"]
     });
 
 
@@ -28,7 +28,7 @@ jQuery(function() {
         autoplay: false,
         dots: false,
         nav: true,
-        navText: ["<img src='https://cdn.statically.io/gh/saeedane/sabaqpress/41fcaed7/img/icons/chevron-right.svg' width='24' height='24'>","<img src='https://cdn.statically.io/gh/saeedane/sabaqpress/41fcaed7/img/icons/chevron-left.svg' width='24' height='24'>"]
+        //navText: ["<img src='https://cdn.statically.io/gh/saeedane/sabaqpress/41fcaed7/img/icons/chevron-right.svg' width='24' height='24'>","<img src='https://cdn.statically.io/gh/saeedane/sabaqpress/41fcaed7/img/icons/chevron-left.svg' width='24' height='24'>"]
     });
 
 
